0335e2d75d30001b9c2c3962ec757dcfc5eeefb4
Tejas Shah <tejasshah2k19@gmail.com>
2026-06-19 10:52:42 +0530

    array impl all fun

--- ArrayDemo.c
+++ ArrayDemo.c
@@ -5,13 +5,32 @@ int arr[SIZE];
 
 void insert(int location, int data)
 {
-    int index = location - 1; 
+    int found = 0; // no
 
-    for(int i=SIZE-1; i>index; i--){
-        arr[i] = arr[i-1];
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (arr[i] == data)
+        {
+            found = 1;//yes 
+            break;
+        }
     }
 
-    arr[index] = data; 
+    if (found == 1)
+    {
+        printf("\n%d is already present in the array ", data);
+    }
+    else
+    {
+        int index = location - 1;
+
+        for (int i = SIZE - 1; i > index; i--)
+        {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[index] = data;
+    }
 }
 
 void display()
@@ -23,19 +42,33 @@ void display()
     }
 }
 
+void removeItem(int location)
+{
+    int index = location - 1;
+
+    for (int i = index; i < SIZE; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+}
+
 int main()
 {
     insert(1, 10);
     insert(2, 20);
     insert(3, 30);
+    insert(4, 40);
+    insert(5, 50);
 
-    display(); // 10 20 30
+    display(); // 10 20 30 40 50
 
-    insert(1, 100);
+    removeItem(1);
 
-    insert(2, 200);
+    display(); // 20 30 40 50
 
-    display(); // 100 200 10 20 30
+    insert(3,50); 
+    insert(3,10); // 
+    display(); //20 30 10 40 50 
 
     return 0;
 }
